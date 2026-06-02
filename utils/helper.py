e836199a49f6e249707c0e33a18313f7f7bfa308
Shlomi <shlomikaufman91@gmail.com>
2026-06-02 20:43:48 +0300

    Fix bugs and add a data soldiers

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,17 @@
 from utils.io import load_json, save_json
-from fastapi import HTTPException
+from logger_config import logger
+
+
+def soldier_by_id(id_: int, soldiers: list[dict]):
+    # if not soldiers:
+    #     raise ("There is not soldiers in data")
+
+    for soldier in soldiers:
+        if soldier["id"] == id_:
+            logger.info(f"Find user with DI: {id_}")
+            return soldier
+    raise KeyError(f"ID: {id_} is not found")
+
 
 def get_all_soldiers():
     soldiers = load_json()
@@ -8,47 +20,34 @@ def get_all_soldiers():
 
 def get_soldier(id_: int):
     soldiers = load_json()
-    
-    if not soldiers:
-        raise HTTPException(404, "There is not soldiers in data")
-    
-    for soldier in soldiers:
-        if soldier["id_Personal"] == id_:
-            return soldier
-    raise HTTPException(500, "ID is not exists")
+    return soldier_by_id(id_, soldiers)
 
 
 def create_soldier(new_soldier: dict):
     soldiers = load_json()
-    soldier_id = max([soldier for soldier in soldiers]) + 1 if soldiers else 1
+    soldier_id = max([soldier["id"] for soldier in soldiers]) + 1 if soldiers else 1
 
     new_soldier = {"id": soldier_id, **new_soldier}
-    soldiers.update(new_soldier)
+    soldiers.append(new_soldier)
 
     save_json(soldiers)
-    # if data:
-    #     return True
-    # return False
+
 
 def update_soldier(id_: int, updated_soldier: dict):
     soldiers = load_json()
     
     for soldier in soldiers:
         if soldier["id"] == id_:
-            soldiers.update(updated_soldier)
+            soldier.update(updated_soldier)
             save_json(soldiers)
-            break
-    else:
-        raise HTTPException(404, "ID soldier is not exists")
-    
+            return
+    raise KeyError
+
 
 def delete_soldier(id_: int):
     soldiers = load_json()
 
-    updated_soldiers = [soldier for soldier in soldiers if soldier["id"] != id_]
-
-    if len(soldiers) == len(updated_soldiers):
-        raise HTTPException(404, f"ID: {id_} is not found")
+    soldier = soldier_by_id(id_, soldiers)
+    soldiers.remove(soldier)
     
-    soldiers.update(updated_soldiers)
     save_json(soldiers)
--- utils/io.py
+++ utils/io.py
@@ -1,18 +1,28 @@
 import os
 import json
 from logger_config import logger
-from fastapi import HTTPException
 
 
 FILENAME = "soldiers.json"
 
-def read_json():
+def load_json():
+    data = []
     if os.path.exists(FILENAME):
         with open(FILENAME, "r", encoding="utf-8") as file:
             data = json.load(file)
             
             if len(data) == 0:
                 logger.warning(f"File {FILENAME} is empty")
-            logger.info(f"Return {len(data)} data")
-            return data or []
-    raise HTTPException(404, "Error file is not exists")
+            logger.info(f"Return {len(data)} soldiers")
+            return data
+    raise FileNotFoundError("Error file is not exists")
+
+
+def save_json(soldiers: list[dict]):
+    if not soldiers:
+        logger.warning("soldiers is empty")
+
+    with open(FILENAME, "w", encoding="utf-8") as f:
+        json.dump(soldiers, f, indent=2)
+    logger.info(f"Save {len(soldiers)} soldiers")
+
