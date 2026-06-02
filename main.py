7a622fc320f5096dca523c1c1bddfdfb9188c3ea
Shlomi <shlomikaufman91@gmail.com>
2026-06-02 18:27:15 +0300

    Add endpoint of update soldier

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from logger_config import logger
 from utils.helper import get_all_soldiers
 from utils.helper import get_soldier
 from utils.helper import create_soldier
+from utils.helper import update_soldier
 from fastapi import FastAPI, HTTPException
 import json
 
@@ -46,4 +47,24 @@ def create_soldier_endpoint(new_soldier: dict):
     
     except json.JSONDecodeError:
         logger.error(f"File json is empty")
-        raise HTTPException(500, "Internal Server Error")
+        raise HTTPException(500, "In Server Error")
+    
+
+@app.put("/api/soldiers/{id_}")
+def update_soldier_endpoint(id_: int, new_soldier: dict):
+    try:
+        logger.info("Try to update file")
+        update_soldier(id_, new_soldier)
+        return {"Message": f"Soldier update successfully {id_}"}
+    
+    except KeyError:
+        logger.warning("ID is not exists")
+        raise HTTPException(400, f"ID: {id_} is not found")
+
+    except FileNotFoundError:
+        logger.error("File is not exists")
+        raise HTTPException(404, "File is not found")
+    
+    except json.JSONDecodeError:
+        logger.error("JSON file is empty")
+        raise HTTPException(500, "Invalid server error")
