e9ccc633956fb5d715bee68b055d46f3b73b65d9
Shlomi <shlomikaufman91@gmail.com>
2026-06-02 12:30:05 +0300

    Add a defintion of logger

---
+++ logger_config.py
@@ -0,0 +1,15 @@
+import logging
+
+
+LOG_FILE = "system.log"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
